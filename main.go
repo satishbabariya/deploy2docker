ec8e184537d2388c6810e5be684a04c39c77a5a3
Satish Babariya <satish.babariya@gmail.com>
2021-12-29 22:27:27 +0530

    add docker build and deploy over ssh

--- main.go
+++ main.go
@@ -25,52 +25,38 @@ SOFTWARE.
 package main
 
 import (
+	"context"
+	"deploy2docker/internal"
 	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
-	"time"
 
+	"github.com/moby/term"
+	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
-	"golang.org/x/crypto/ssh"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
+	docker "github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/archive"
+	"github.com/docker/docker/pkg/jsonmessage"
+	"github.com/docker/docker/pkg/stdcopy"
+	"github.com/docker/go-connections/nat"
 )
 
 func main() {
-	var address string
-	var username string
-	var key string
 
 	app := &cli.App{
 		Name:  "Deploy To Docker",
 		Usage: "Deploy to Remote Docker using SSH",
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:        "address",
-				Usage:       "host address with port",
-				Required:    true,
-				Destination: &address,
-			},
-			&cli.StringFlag{
-				Name:        "user",
-				Usage:       "host server user",
-				Required:    true,
-				Destination: &username,
-			},
-			&cli.StringFlag{
-				Name:        "key",
-				Usage:       "host server ssh key",
-				Required:    true,
-				Destination: &key,
-			},
-		},
-
 		Action: func(c *cli.Context) error {
-			fmt.Println("root action")
-
-			client, err := ConnectWithKey(address, username, key)
+			sshclient := internal.NewSSHClient("ec2-3-22-224-209.us-east-2.compute.amazonaws.com:22", "ec2-user")
+			client, err := sshclient.ConnectWithKey("/Users/satishbabariya/Desktop/deployit.pem")
 			if err != nil {
-				log.Fatal(err)
+				logrus.Errorln(fmt.Errorf("error while connecting to remote: %v", err))
 				return err
 			}
 			defer client.Close()
@@ -78,35 +64,56 @@ func main() {
 			// Establish connection with docker
 			remote, err := client.Dial("unix", "/var/run/docker.sock")
 			if err != nil {
-				log.Fatalln(err)
+				logrus.Errorln(fmt.Errorf("error while connecting to remote docker: %v", err))
+				return err
 			}
+			defer remote.Close()
 
-			const SockAddr = "/tmp/docker.sock"
-			if err := os.RemoveAll(SockAddr); err != nil {
-				log.Fatal(err)
+			logrus.Infoln("Connected to remote docker")
+
+			const SocketAddress = "/tmp/docker.sock"
+			if _, err := os.Stat(SocketAddress); err == nil {
+				if err := os.RemoveAll(SocketAddress); err != nil {
+					return err
+				}
 			}
 
 			os.Setenv("DOCKER_HOST", "unix:///tmp/docker.sock")
 
 			// Start local server to forward traffic to remote connection
-			local, err := net.Listen("unix", SockAddr)
+			local, err := net.Listen("unix", SocketAddress)
 			if err != nil {
-				log.Fatalln(err)
+				logrus.Errorln(fmt.Errorf("error while listening to local unix socks: %v", err))
 				return err
 			}
 			defer local.Close()
 
-			fmt.Println(local)
-
 			// Handle incoming connections
-			for {
-				client, err := local.Accept()
-				if err != nil {
-					log.Fatalln(err)
-					return err
+			go func() {
+				for {
+					client, err := local.Accept()
+					if err != nil {
+						logrus.Fatalln(err)
+						return
+					}
+					handleClient(client, remote)
 				}
-				fmt.Println(client)
-				handleClient(client, remote)
+			}()
+
+			cli, err := docker.NewClientWithOpts(docker.FromEnv)
+			if err != nil {
+				return err
+			}
+
+			err = BuildAndDeploy(c.Context, cli)
+			if err != nil {
+				return err
+			}
+
+			logrus.Infoln("Deployed to remote docker")
+			err = RunContainer(c.Context, cli)
+			if err != nil {
+				return err
 			}
 
 			return nil
@@ -115,53 +122,12 @@ func main() {
 
 	err := app.Run(os.Args)
 	if err != nil {
-		log.Fatal(err)
+		logrus.Fatal(err)
 	}
-}
-
-func ConnectWithKey(addr, user, key string) (*ssh.Client, error) {
-	//signer, err := ssh.ParsePrivateKeyWithPassphrase(key, []byte("password"))
-	signer, err := ssh.ParsePrivateKey([]byte(key))
-	if err != nil {
-		log.Fatalf("unable to parse private key: %v", err)
-	}
-
-	sshConfig := &ssh.ClientConfig{
-		User: user,
-		Auth: []ssh.AuthMethod{
-			ssh.PublicKeys(signer),
-		},
-		HostKeyCallback: ssh.HostKeyCallback(func(hostname string, remote net.Addr, key ssh.PublicKey) error { return nil }),
-	}
-
-	conn, err := ssh.Dial("tcp", addr, sshConfig)
-	if err != nil {
-		return nil, err
-	}
-
-	return conn, nil
-}
-
-func Connect(addr, user, password string) (*ssh.Client, error) {
-	sshConfig := &ssh.ClientConfig{
-		User: user,
-		Auth: []ssh.AuthMethod{
-			ssh.Password(password),
-		},
-		Timeout:         30 * time.Second,
-		HostKeyCallback: ssh.HostKeyCallback(func(hostname string, remote net.Addr, key ssh.PublicKey) error { return nil }),
-	}
-
-	conn, err := ssh.Dial("tcp", addr, sshConfig)
-	if err != nil {
-		return nil, err
-	}
-
-	return conn, nil
 }
 
 func handleClient(client net.Conn, remote net.Conn) {
-	defer client.Close()
+	// defer client.Close()
 	chDone := make(chan bool)
 
 	// Start remote -> local data transfer
@@ -183,4 +149,126 @@ func handleClient(client net.Conn, remote net.Conn) {
 	}()
 
 	<-chDone
+}
+
+func RunContainer(ctx context.Context, cli *docker.Client) error {
+
+	// stop old container if exists and remove it
+	err := cli.ContainerRemove(ctx, "go-echo-example", types.ContainerRemoveOptions{
+		Force: true,
+	})
+	if err != nil {
+		logrus.Errorln(fmt.Errorf("error while removing old container: %v", err))
+		return err
+	}
+
+	// Create a new container
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "satishbabariya/go-echo-example:latest",
+		ExposedPorts: nat.PortSet{
+			"8080/tcp": struct{}{},
+		},
+		AttachStdout: true,
+	}, &container.HostConfig{
+		PortBindings: nat.PortMap{
+			"8080/tcp": []nat.PortBinding{
+				{
+					HostIP:   "0.0.0.0",
+					HostPort: "8080",
+				},
+			},
+		},
+	}, &network.NetworkingConfig{}, nil, "go-echo-example")
+	if err != nil {
+		return err
+	}
+
+	// Start the container
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		return err
+	}
+
+	// Wait for the container to finish
+	// statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	// select {
+	// case err := <-errCh:
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// case <-statusCh:
+	// }
+
+	// Inspect the container
+	// inspect, err := cli.ContainerInspect(ctx, resp.ID)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// // Print the container's IP address
+	// fmt.Println(inspect)
+
+	// get container logs
+	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	if err != nil {
+		return err
+	}
+
+	io.Copy(os.Stdout, out)
+	defer out.Close()
+
+	return nil
+}
+
+func BuildAndDeploy(ctx context.Context, cli *docker.Client) error {
+	tar, err := archive.TarWithOptions("/Users/satishbabariya/Desktop/go-echo-example", &archive.TarOptions{})
+	if err != nil {
+		logrus.Errorln(fmt.Errorf("error while creating tar: %v", err))
+		return err
+	}
+
+	defer tar.Close()
+
+	// build docker image
+	resp, err := cli.ImageBuild(ctx, tar, types.ImageBuildOptions{
+		Tags:   []string{"satishbabariya/go-echo-example:latest"},
+		Remove: true,
+	})
+	if err != nil {
+		logrus.Errorln(fmt.Errorf("error while building image: %v", err))
+		return err
+	}
+
+	// _, err = io.Copy(logrus.New().Out, resp.Body)
+	// if err != nil {
+	// 	logrus.Errorln(fmt.Errorf("error while copying build output: %v", err))
+	// 	return err
+	// }
+
+	termFd, isTerm := term.GetFdInfo(os.Stderr)
+	jsonmessage.DisplayJSONMessagesStream(resp.Body, os.Stderr, termFd, isTerm, nil)
+
+	defer resp.Body.Close()
+
+	logrus.Infoln("Built docker image satishbabariya/go-echo-example")
+
+	return nil
+}
+
+func LogContainer(ctx context.Context, cli *docker.Client, resp container.ContainerCreateCreatedBody) error {
+	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			return err
+		}
+	case <-statusCh:
+	}
+
+	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	if err != nil {
+		return err
+	}
+
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+	return nil
 }
